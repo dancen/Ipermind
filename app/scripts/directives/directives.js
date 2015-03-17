c28eefe9034add3b491d3196c142c87999908fab
dan <dan@dan-VGN-FW41J-H>
2015-03-17 21:46:27 +0100

    first commit

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -2,34 +2,6 @@
 
     var ipermindDirectives = angular.module('ipermindDirectives', []);
 
-    ipermindDirectives.directive("spinnerLoader", function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'app/scripts/views/partials/spinner-loader.html',
-            replace: true
-        };
-    });
-    
-    ipermindDirectives.directive("layoutFooter", function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'app/scripts/views/partials/layout-footer.html',
-            replace: true
-        };
-    });
-    
-    ipermindDirectives.directive("layoutHeader", function() {
-        return {
-            restrict: 'E',
-            templateUrl: 'app/scripts/views/partials/layout-header.html',
-            replace: true
-        };
-    });
-    
-    
-    
-    
-
-
+     
 
 })();
