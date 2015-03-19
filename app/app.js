018ac46484c7112455ffd1c9a6341f77e6798e68
dan <dan@dan-VGN-FW41J-H>
2015-03-19 07:52:46 +0100

    first commit

--- app/app.js
+++ app/app.js
@@ -8,31 +8,15 @@
 
 
     // MAIN MODULE CREATION
-    var daniele = angular.module('daniele', [
-        'danieleControllers',
+    var danieleApp = angular.module('danieleApp', [
+        'calculatorControllers',
+        'ipermindControllers',
         'ipermindFactories',
         'ipermindDirectives',
-        'ipermindServices'])
-            .constant('_KING', "king")
-            .constant('_QUEEN', "queen")
-            .constant('_ROOK', "rook")
-            .constant('_BISHOP', "bishop")
-            .constant('_KNIGHT', "knight")
-            .constant('_PAWN', "pawn")
-            .constant('_EMPTY', "e")
-            .constant('_BLACK', "b")
-            .constant('_WHITE', "w")
-            .constant('_PARSED', "p")
-            .constant('LEVEL_EASY', 1)
-            .constant('LEVEL_MEDIUM', 2)
-            .constant('LEVEL_DIFFICULT', 3)
-            .constant('COUNTDOWN', 120)
-            .constant('_ARROW', "arrow")
-            .constant('_ARROW_EMPTY', "arrow_empty")
-            .constant('_SEQ_EMPTY', "empty")
-            .constant('RESPONSE_SUCCESS', "success");
+        'ipermindServices']);
 
 
+   
     
 
 
